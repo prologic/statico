72a7d52a685ab45378714debe438e5ccc5174a87
oss6 <ossedb@gmail.com>
2015-05-31 17:47:59 +0100

    Added requirements + preview + fixes

--- statico/statico.py
+++ statico/statico.py
@@ -9,6 +9,8 @@ import markdown
 import json
 import shutil
 from datetime import date
+import http.server
+import socketserver
 import argparse as ap
 import jinja2 as jn
 
@@ -34,6 +36,22 @@ def copy_directory(src, dest):
     # Any error saying that the directory doesn't exist
     except OSError as e:
         print('Directory not copied. Error: %s' % e)
+
+
+def run_server():
+    print('cd output')
+    os.chdir('output')
+
+    PORT = 8000
+    Handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", PORT), Handler)
+
+    print("Serving at port...", PORT)
+    httpd.serve_forever()
+
+
+def deploy():
+    pass
 
 
 def parse_metadata(fp):
@@ -73,6 +91,7 @@ def parse_index(filename, o):
     html = markdown.markdown(''.join(rest))
     data['articles'] = get_articles(o.get('articles'))
     data['content'] = html
+    data['site'] = o.get('settings')
 
     template = o.get('env').get_template('default.html')
     page = template.render(data)
@@ -99,20 +118,19 @@ def get_articles(f_articles):
 
 
 def create():
-    settings = open('settings.json', 'w')
+    path = os.path.abspath(__file__)
+    dir_path = os.path.dirname(path)
+
+    # Settings
+    shutil.copy(os.path.join(dir_path, 'settings.json'), 'settings.json')
 
     # Static
-    os.makedirs('static')
-    os.makedirs(os.path.join('static', 'css'))
-    os.makedirs(os.path.join('static', 'js'))
-    os.makedirs(os.path.join('static', 'images'))
-    style = open(os.path.join('static', 'css', 'main.css'), 'w')
-    js = open(os.path.join('static', 'js', 'main.js'), 'w')
+    static_path = os.path.join(dir_path, 'static')
+    copy_directory(static_path, 'static')
 
     # Templates
-    path = os.path.abspath(__file__)
-    dir_path = os.path.join(os.path.dirname(path), 'templates')
-    copy_directory(dir_path, 'templates')
+    templates_path = os.path.join(dir_path, 'templates')
+    copy_directory(templates_path, 'templates')
 
     # Content
     os.makedirs('content')
@@ -143,8 +161,7 @@ def new_page(name):
         'title: ' + name + '\n',
         'date: ' + date.today().isoformat() + '\n',
         'author: ' + settings.get('author') + '\n',
-        'summary: A beautiful page\n',
-        '---'
+        '---\n'
     ])
     page.close()
 
@@ -166,26 +183,24 @@ def new_article(title):
         'date: ' + date.today().isoformat() + '\n',
         'author: ' + settings.get('author') + '\n',
         'summary: A beautiful page\n',
-        '---'
+        '---\n'
     ])
 
 
 def generate():
-    # Go into 'content' and retrieve all files
-    # Parse metadata from files
-    # Convert the rest of file into html
-    # Output into another directory, respecting their metadata.
+    settings = json.load(open('settings.json'))
 
     # Get all filenames
-    # files = [os.path.join('content', 'index.html')]
     pages = [os.path.join('content', 'pages', f) for f in os.listdir(os.path.join('content', 'pages'))]
     articles = [os.path.join('content', 'articles', f) for f in os.listdir(os.path.join('content', 'articles'))]
 
     files = pages + articles
 
     # BEGIN: Parse files
-    env = jn.Environment(loader=jn.PackageLoader('statico', 'templates'))
+    loader = jn.FileSystemLoader(os.path.join(os.getcwd(), 'templates'))
+    env = jn.Environment(loader=loader)
 
+    print(' - Parsing articles and pages')
     for f in files:
         fp = open(f)
         file_no_ext = os.path.basename(os.path.normpath(f.split('.')[0]))
@@ -203,16 +218,19 @@ def generate():
         template = env.get_template(layout + '.html')
 
         data['content'] = html
+        data['site'] = settings
         page = template.render(data)  # Date and other things
 
         open(os.path.join('output', target, file_no_ext + '.html'), 'w').write(page)
 
     # Copy index
-    parse_index(os.path.join('content', 'index.md'), {'env': env, 'articles': articles})
+    print(' - Parsing index page')
+    parse_index(os.path.join('content', 'index.md'), {'env': env, 'settings': settings, 'articles': articles})
 
     # END: Parse files
 
     # Copy static directory
+    print(' - Generating static directory')
     static = os.path.join('output', 'static')
     if os.path.exists(static):
         shutil.rmtree(static)
@@ -231,11 +249,15 @@ def main():
         parser.add_argument('-p', '--page', help='Create a page', type=str)
         parser.add_argument('-a', '--article', help='Create an article', type=str)
         parser.add_argument('-c', '--clear', help='Clear directory', action='store_true')
-        parser.add_argument('-d', '--deploy', help='Deploy', action='store_true')
+        parser.add_argument('-P', '--preview', help='Preview your site', action='store_true')
+        parser.add_argument('-d', '--deploy', help='Deploy to GitHub', action='store_true')
         args = parser.parse_args()
 
         if args.generate:
+            print('Generating site...')
             generate()
+            print('Head to "output" to view your generated site.\n'
+                  'Type "statico --deploy" to deploy your site to GitHub, otherwise upload manually.')
         elif args.page:
             new_page(args.page)
         elif args.article:
@@ -243,5 +265,7 @@ def main():
         elif args.clear:
             if input('Are you sure you want to clear the workspace? [y/n] ') == 'y':
                 clear_workspace()
+        elif args.preview:
+            run_server()
         elif args.deploy:
-            pass
+            deploy()
