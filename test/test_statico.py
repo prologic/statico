d0c4a3aaaa7674a5fc639527af035b207f22bddf
oss6 <ossedb@gmail.com>
2015-06-13 16:05:13 +0100

    Testing

---
+++ test/test_statico.py
@@ -0,0 +1,11 @@
+import unittest
+import statico.statico as st
+
+
+class StaticoTest(unittest.TestCase):
+
+    def test_parse_metadata(self):
+        pass
+
+    def test_parse_index(self):
+        pass
